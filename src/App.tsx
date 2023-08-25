d5438a8f42a2e37b35d5fed6eeff8d570f634ed4
NangmanCo <admin@nangman.io>
2023-08-25 13:49:26 +0900

    지도 부분 app,index 부분 2개 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,16 @@ import { useEffect } from 'react';
 
 function App() {
   const content = useRoutes(router);
-  // useEffect(() => {
-  //   const script = document.createElement('script');
-  //   script.src = `http://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.REACT_APP_NAVER_MAP_CLIENT_ID}`;
-  //   script.async = true;
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = `http://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.REACT_APP_NAVER_MAP_CLIENT_ID}`;
+    script.async = true;
 
-  //   document.head.appendChild(script);
-  //   return () => {
-  //     document.head.removeChild(script);
-  //   };
-  // }, []);
+    document.head.appendChild(script);
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
   return <div>{content}</div>;
 }
 export default App;
