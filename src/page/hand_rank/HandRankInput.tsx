a46e1b80c427cffcaa177be6e23845a91ca7e2ec
NangmanCo <admin@nangman.io>
2023-08-28 19:55:58 +0900

    핸드 순위 수정

--- src/page/hand_rank/HandRankInput.tsx
+++ src/page/hand_rank/HandRankInput.tsx
@@ -3,9 +3,18 @@ import basic from '../../utils/basic.json';
 import Card from '../../component/Card';
 import CardSetDialog from '../../component/CardSetDialog';
 import handData from './hand_data.json';
-
+import handData2 from './hand_data2.json';
+import handData3 from './hand_data3.json';
+import handData4 from './hand_data4.json';
+import handData6 from './hand_data6.json';
+import handData8 from './hand_data8.json';
+import handData10 from './hand_data.json';
 interface HandRankInputProps {}
-
+type SelectedData = {
+  index: number;
+  card: string;
+  sum: number;
+}[];
 const HandRankInput: React.FC<HandRankInputProps> = () => {
   const [isSuited, setIsSuited] = useState(false);
 
@@ -15,7 +24,132 @@ const HandRankInput: React.FC<HandRankInputProps> = () => {
   const [cards, setCards] = useState<string[]>(['', '']);
 
   const [rank, setRank] = useState<string>('0');
+  const [selectedBtn, setSelectedBtn] = useState('2');
+  const [selectedData, setSelectedData] = useState(handData2);
+  const handleButtonClick = (name: string) => {
+    setSelectedBtn(name);
+  };
+  const handleButtonData = (name: string) => {
+    switch (name) {
+      case '2':
+        setSelectedData(handData2);
+        setCards((prev) => {
+          updateRank(prev[0], prev[1], handData2);
 
+          return prev;
+        });
+
+        break;
+      case '3':
+        setSelectedData(handData3);
+        setCards((prev) => {
+          updateRank(prev[0], prev[1], handData3);
+
+          return prev;
+        });
+        break;
+      case '4~5':
+        setSelectedData(handData4);
+        setCards((prev) => {
+          updateRank(prev[0], prev[1], handData4);
+
+          return prev;
+        });
+        break;
+      case '6~7':
+        setSelectedData(handData6);
+        setCards((prev) => {
+          updateRank(prev[0], prev[1], handData6);
+
+          return prev;
+        });
+        break;
+      case '8~9':
+        setSelectedData(handData8);
+        setCards((prev) => {
+          updateRank(prev[0], prev[1], handData8);
+
+          return prev;
+        });
+        break;
+      case '10':
+        setSelectedData(handData10);
+        setCards((prev) => {
+          updateRank(prev[0], prev[1], handData10);
+
+          return prev;
+        });
+        break;
+
+      default:
+        setSelectedData(handData2);
+        setCards((prev) => {
+          updateRank(prev[0], prev[1], handData2);
+
+          return prev;
+        });
+        break;
+    }
+  };
+  interface XwayProps {
+    value: string;
+    handleButtonClick: React.Dispatch<React.SetStateAction<string>>;
+    handleButtonData: React.Dispatch<React.SetStateAction<string>>;
+  }
+  const Xway: React.FC<XwayProps> = (props) => {
+    return (
+      <div className="grid grid-cols-3 gap-1   text-2xl m-2 w-2/3  ">
+        <BasicBtn
+          name="2"
+          isSel={props.value === '2'}
+          onClick={() => {
+            props.handleButtonClick('2');
+            props.handleButtonData('2');
+          }}
+        ></BasicBtn>
+        <BasicBtn
+          name="3"
+          isSel={props.value === '3'}
+          onClick={() => {
+            props.handleButtonClick('3');
+            props.handleButtonData('3');
+          }}
+        ></BasicBtn>
+        <BasicBtn
+          name="4~5"
+          isSel={props.value === '4~5'}
+          onClick={() => {
+            props.handleButtonClick('4~5');
+            props.handleButtonData('4~5');
+          }}
+        ></BasicBtn>
+        <BasicBtn
+          name="6~7"
+          isSel={props.value === '6~7'}
+          onClick={() => {
+            props.handleButtonClick('6~7');
+            props.handleButtonData('6~7');
+          }}
+        ></BasicBtn>
+        <BasicBtn
+          name="8~9"
+          isSel={props.value === '8~9'}
+          onClick={() => {
+            props.handleButtonClick('8~9');
+            props.handleButtonData('8~9');
+          }}
+        ></BasicBtn>
+        <BasicBtn
+          name="10"
+          isSel={props.value === '10'}
+          onClick={() => {
+            props.handleButtonClick('10');
+            props.handleButtonData('10');
+          }}
+        ></BasicBtn>
+      </div>
+    );
+  };
   const isCard = (card: string): boolean => {
     // c2, dt, ha, s9 : true,  s1, tt, eq : false
     if (
@@ -70,7 +204,7 @@ const HandRankInput: React.FC<HandRankInputProps> = () => {
           prevCards[0] = `s${cardNumber}`;
           prevCards[1] = `d${cardNumber}`;
           setIsSuited(false);
-          updateRank(prevCards[0], prevCards[1]);
+          updateRank(prevCards[0], prevCards[1], selectedData);
           return prevCards;
         }
       }
@@ -85,7 +219,7 @@ const HandRankInput: React.FC<HandRankInputProps> = () => {
         }
       }
 
-      updateRank(prevCards[0], prevCards[1]);
+      updateRank(prevCards[0], prevCards[1], selectedData);
       return prevCards;
     });
   };
@@ -102,7 +236,7 @@ const HandRankInput: React.FC<HandRankInputProps> = () => {
         prev[0] = isCard(prev[0]) ? `s${prev[0].charAt(1)}` : '';
         prev[1] = isCard(prev[1]) ? `d${prev[1].charAt(1)}` : '';
       }
-      updateRank(prev[0], prev[1]);
+      updateRank(prev[0], prev[1], selectedData);
 
       return prev;
     });
@@ -125,7 +259,7 @@ const HandRankInput: React.FC<HandRankInputProps> = () => {
     return cardText;
   };
 
-  const updateRank = (card1: string, card2: string) => {
+  const updateRank = (card1: string, card2: string, dataList: SelectedData) => {
     if (isCard(card1) && isCard(card2)) {
       let cardsText = '';
       if (isPocket()) {
@@ -140,9 +274,9 @@ const HandRankInput: React.FC<HandRankInputProps> = () => {
       cardsText = transToRealCardText(cardsText);
 
       let percentage: string = '0';
-      for (let i = 0; i < handData.length; i++) {
-        if (handData[i].card === cardsText) {
-          percentage = handData[i].sum.toString();
+      for (let i = 0; i < dataList.length; i++) {
+        if (dataList[i].card === cardsText) {
+          percentage = dataList[i].sum.toString();
           break;
         }
       }
@@ -168,6 +302,15 @@ const HandRankInput: React.FC<HandRankInputProps> = () => {
             );
           })}
         </div>
+        <div className="flex justify-center">
+          {' '}
+          <Xway
+            value={selectedBtn}
+            handleButtonClick={handleButtonClick}
+            handleButtonData={handleButtonData}
+          />
+        </div>
+
         <TypeSel isSuited={isSuited} setIsSuited={suitCardChange} />
         <ResultRank rank={rank} />
       </div>
