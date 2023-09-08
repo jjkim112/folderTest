28eeabd12121255d0ef6da241684db17a51e09e3
NangmanCo <admin@nangman.io>
2023-09-08 14:04:47 +0900

    230908

--- src/domain/Game.model.ts
+++ src/domain/Game.model.ts
@@ -1,19 +1,5 @@
-<<<<<<< HEAD
 import { Timestamp } from "firebase/firestore";
-
-export class Game {
-  map(
-    arg0: (game: any, i: any) => import("react/jsx-runtime").JSX.Element
-  ): import("react").ReactNode {
-    throw new Error("Method not implemented.");
-  }
-  readonly id: string;
-  readonly pubId: string;
-  readonly gameTempId: string;
-
-=======
-import { Timestamp } from 'firebase/firestore';
-import firebase from 'firebase/compat/app';
+import firebase from "firebase/compat/app";
 export class Game {
   id: string;
   pubId: string;
@@ -22,7 +8,6 @@ export class Game {
   subTitle: string;
   description: string;
   note: string;
->>>>>>> 5916c20553f7399125b357b11425623a109cd518
   entry: number;
   date: Date;
   totalReward: string;
@@ -73,26 +58,26 @@ export class Game {
 
   get toMap() {
     return {
-      id: this.id || '',
-      pubId: this.pubId || '',
-      gameTempId: this.gameTempId || '',
-      title: this.title || '',
-      subTitle: this.subTitle || '',
-      description: this.description || '',
-      note: this.note || '',
+      id: this.id || "",
+      pubId: this.pubId || "",
+      gameTempId: this.gameTempId || "",
+      title: this.title || "",
+      subTitle: this.subTitle || "",
+      description: this.description || "",
+      note: this.note || "",
       entry: this.entry || 0,
       date: firebase.firestore.Timestamp.fromDate(this.date),
-      totalReward: this.totalReward || '',
+      totalReward: this.totalReward || "",
       players:
         this.players && Array.isArray(this.players)
           ? // Make sure each player object does not contain undefined values
             // If the player object is not defined, assign an empty array
             this.players.map((v) => ({
-              id: v.id || '',
-              name: v.name || '',
-              prize: v.prize || '',
-              note: v.note || '',
-              rank: v.rank || '',
+              id: v.id || "",
+              name: v.name || "",
+              prize: v.prize || "",
+              note: v.note || "",
+              rank: v.rank || "",
             }))
           : [],
     };
@@ -100,26 +85,17 @@ export class Game {
 
   static fromData(data: any): Game {
     try {
-<<<<<<< HEAD
       const id = data["id"];
       const pubId = data["pubId"];
       const gameTempId = data["gameTempId"];
+      const title = data["title"];
+      const subTitle = data["subTitle"];
+      const description = data["description"];
+      const note = data["note"];
       const entry = data["entry"];
       const date = (data["date"] as Timestamp).toDate();
+      const totalReward = data["totalReward"];
       const players = data["players"];
-=======
-      const id = data['id'];
-      const pubId = data['pubId'];
-      const gameTempId = data['gameTempId'];
-      const title = data['title'];
-      const subTitle = data['subTitle'];
-      const description = data['description'];
-      const note = data['note'];
-      const entry = data['entry'];
-      const date = (data['date'] as Timestamp).toDate();
-      const totalReward = data['totalReward'];
-      const players = data['players'];
->>>>>>> 5916c20553f7399125b357b11425623a109cd518
 
       return new Game(
         id,
@@ -136,11 +112,7 @@ export class Game {
       );
     } catch (error) {
       console.log(`[GameTemplate Model] fromData e: ${error}`);
-<<<<<<< HEAD
-      return new Game("", "", "", 0, new Date(0), []);
-=======
-      return new Game('', '', '', '', '', '', '', 0, new Date(0), '', []);
->>>>>>> 5916c20553f7399125b357b11425623a109cd518
+      return new Game("", "", "", "", "", "", "", 0, new Date(0), "", []);
     }
   }
 }
